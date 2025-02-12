45eeb3bbc22ac1beadbf6a570547bd84b74adf7d
AnishRM-ai <anishmagar228@gmail.com>
2025-02-12 21:11:56 +0545

    created api endpoints for onboarding for both client and therapist.

--- backend/config/googleOAuth.js
+++ backend/config/googleOAuth.js
@@ -1,33 +1,44 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User');
+const Client = require('../models/clientdb'); // Import the Client model
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK_URL,
-    passReqToCallback: true,  // Allows access to `req` in the callback
-    state: true // Ensures CSRF protection
+    passReqToCallback: true,
+    state: true
   },
   async (req, accessToken, refreshToken, profile, done) => {
     try {
-       // Extract role from state query
-      let role = 'client';
-     
+      let role = 'client';  // Default role is client
 
       let user = await User.findOne({ email: profile.emails[0].value });
 
       if (!user) {
-        user = new User({
-          username: profile.emails[0].value.split('@')[0],
-          fullname: profile.displayName,
-          email: profile.emails[0].value,
-          oauthProvider: 'google',
-          oauthId: profile.id,
-          role:role,
-          isVerified: true
-        });
-
+        if (role === 'client') {
+          user = new Client({  // Create a Client instead of a User
+            username: profile.emails[0].value.split('@')[0],
+            fullname: profile.displayName,
+            email: profile.emails[0].value,
+            oauthProvider: 'google',
+            oauthId: profile.id,
+            role: role,
+            isVerified: true
+          });
+        } else {
+          user = new User({
+            username: profile.emails[0].value.split('@')[0],
+            fullname: profile.displayName,
+            email: profile.emails[0].value,
+            oauthProvider: 'google',
+            oauthId: profile.id,
+            role: role,
+            isVerified: true
+          });
+        }
+        
         await user.save();
       }
 
